df0dfa267ea2937d345682c7bfc184e58509603e
Ren <neraj.mendoza@yahoo.com>
2025-10-13 13:12:56 +0800

    add customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,27 @@
+import Table from '@/app/ui/customers/table';
+import { Suspense } from 'react';
 import { Metadata } from 'next';
+import { fetchFilteredCustomers } from '@/app/lib/data';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <>
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <Table customers={customers} />
+      </Suspense>
+    </>
+  );
 }
